7423a0245057a8b09be284bdda38b1c4278d7c94
Mukul V <mukulve1@gmail.com>
2023-06-07 13:43:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # web scraping
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 # html parsing
@@ -12,8 +14,9 @@ from rich.progress import track
 from rich.console import Console
 from rich.table import Table
 
-# intantiate global variables
-df = pd.DataFrame(columns=['Title', 'Location', 'Company', 'Link'])
+# instantiate global variables
+df = pd.DataFrame(columns=['Title', 'Location',
+                  'Company', 'Link', 'Description'])
 console = Console()
 table = Table(show_header=True, header_style="bold")
 
@@ -24,71 +27,31 @@ console.print("Enter Job Location :", style="bold green", end=" ")
 inputJobLocation = input()
 
 
-async def scrapeBebee():
+async def scrapeJobDescription(url):
     global df
-    global inputJobTitle
-    global inputJobLocation
 
+    driver = DriverOptions()
+
+    driver.get(url)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    try:
+        jobDescription = soup.find(
+            "div", class_="show-more-less-html__markup").text.strip()
+        return jobDescription
+    except:
+        return ""
+
+
+def DriverOptions():
     options = Options()
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--headless=new")
 
     driver = webdriver.Chrome(options=options)
-    driver.get(
-        f"https://ca.bebee.com/jobs?term={inputJobTitle}&location={inputJobLocation}")
-
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-
-    ulElement = soup.find('ul', class_='jobs_list_scroll')
-    liElements = ulElement.find_all('li')
-
-    for item in track(liElements, description="Bebee"):
-        jobTitle = item.find('h2', class_='mb-0').text.strip()
-
-        # TODO
-        jobCompany = ""
-        jobLocation = ""
-        jobLink = ""
-
-        df = pd.concat([df, pd.DataFrame({'Title': [jobTitle], 'Location': [
-                       jobLocation], 'Company': [jobCompany], 'Link': [
-            jobLink]})], ignore_index=True)
-
-    driver.quit()
-
-
-async def scrapeEluta():
-    global df
-    global inputJobTitle
-    global inputJobLocation
-
-    options = Options()
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--headless=new")
-
-    driver = webdriver.Chrome(options=options)
-    driver.get(
-        f"https://www.eluta.ca/search?q={inputJobTitle}&l={inputJobLocation}&qc=")
-
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-
-    jobList = soup.findAll('div', class_='organic-job')
-
-    for job in track(jobList, description="Eluta"):
-        jobTitle = job.find('h2', class_='title').text.strip()
-        jobLocation = job.find(
-            'span', class_='location').text.strip()
-        jobCompany = job.find(
-            'a', class_='employer').text.strip()
-        jobLink = job.find('a', class_='lk-job-title')['href']
-
-        df = pd.concat([df, pd.DataFrame({'Title': [jobTitle], 'Location': [
-                       jobLocation], 'Company': [jobCompany], 'Link': [
-            jobLink]})], ignore_index=True)
-
-    driver.quit()
+    return driver
 
 
 async def scrapeLinkedin():
@@ -96,51 +59,63 @@ async def scrapeLinkedin():
     global inputJobTitle
     global inputJobLocation
 
-    options = Options()
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--headless=new")
+    driver = DriverOptions()
 
-    driver = webdriver.Chrome(options=options)
-    driver.get(
-        f"https://www.linkedin.com/jobs/search/?&keywords={inputJobTitle}&location={inputJobLocation}&refresh=true")
+    counter = 0
+    pageCounter = 1
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
+    while True:
+        try:
+            driver.get(
+                f"https://www.linkedin.com/jobs/search/?&keywords={inputJobTitle}&location={inputJobLocation}&refresh=true&start={counter}")
 
-    ulElement = soup.find('ul', class_='jobs-search__results-list')
-    liElements = ulElement.find_all('li')
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
 
-    for item in track(liElements, description="Linkedin"):
-        jobTitle = item.find(
-            'h3', class_='base-search-card__title').text.strip()
-        jobLocation = item.find(
-            'span', class_='job-search-card__location').text.strip()
-        jobCompany = item.find(
-            "h4", class_="base-search-card__subtitle").text.strip()
-        jobLink = ""
+            ulElement = soup.find('ul', class_='jobs-search__results-list')
+            liElements = ulElement.find_all('li')
 
-        df = pd.concat([df, pd.DataFrame({'Title': [jobTitle], 'Location': [
-                       jobLocation], 'Company': [jobCompany], 'Link': [
-            jobLink]})], ignore_index=True)
+            for item in track(liElements, description=f"Linkedin - Page: {pageCounter}"):
+                jobTitle = item.find(
+                    'h3', class_='base-search-card__title').text.strip()
+                jobLocation = item.find(
+                    'span', class_='job-search-card__location').text.strip()
+                jobCompany = item.find(
+                    "h4", class_="base-search-card__subtitle").text.strip()
+                jobLink = item.find_all("a")[0]['href']
+
+                jobDescription = await scrapeJobDescription(jobLink)
+
+                if jobTitle and jobLocation and jobCompany and jobLink:
+                    df = pd.concat([df, pd.DataFrame({'Title': [jobTitle], 'Location': [
+                        jobLocation], 'Company': [jobCompany], 'Link': [
+                        jobLink], 'Description': [jobDescription]})])
+
+            console.print("Scrape Next Page? (y/n) :",
+                          style="bold yellow", end=" ")
+            continueInput = input()
+
+            if continueInput == "n":
+                break
+
+            counter += 25
+            pageCounter += 1
+
+        except:
+            break
 
     driver.quit()
 
 
 async def main():
-    # call all scraping functions
-    tasks = [
-        asyncio.create_task(scrapeEluta()),
-        asyncio.create_task(scrapeLinkedin()),
-        asyncio.create_task(scrapeBebee())
-    ]
-    # wait for all scraping functions to finish
-    await asyncio.gather(*tasks)
+    await scrapeLinkedin()
 
     # create table
     table.add_column("Title")
     table.add_column("Company")
     table.add_column("Location")
     table.add_column("Link")
+    table.add_column("Description")
     # loop over dataframe and print rich table
     for index, row in df.iterrows():
         table.add_row(
@@ -148,6 +123,7 @@ async def main():
             f"{row['Company']}",
             f"{row['Location']}",
             f"{row['Link']}",
+            f"{(row['Description'])[:20]}..."
         )
 
     console.print(table)
